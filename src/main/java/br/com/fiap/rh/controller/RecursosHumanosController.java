2f78ee2e7104184402ff903a659943d5b3adf413
Wellington <well.moriera@live.com>
2022-05-06 13:54:53 -0300

    implementacao do pattern de observabilitty e mais alguns comentarios pra documentacao dos patterns de SOLID e Distributed tracing

--- src/main/java/br/com/fiap/rh/controller/RecursosHumanosController.java
+++ src/main/java/br/com/fiap/rh/controller/RecursosHumanosController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.fiap.rh.model.Funcionario;
 import br.com.fiap.rh.repository.FuncionarioRepository;
@@ -26,7 +25,7 @@ import br.com.fiap.rh.service.reajuste.ValidacaoReajuste;
 
 @RestController
 public class RecursosHumanosController {
-	
+
 	/*
 	 * princípio da responsabilidade única, o princípio aberto e fechado e o
 	 * princípio da substituição de Liskov e o princípio da segregação de interface
@@ -35,51 +34,52 @@ public class RecursosHumanosController {
 	 * de orientação a objetos.
 	 * 
 	 * 
-	 * SOLID visa escrever um código que é fácil de manter e principalmente fácil de estender,
-	 * de adicionar novas características, novos comportamentos, novas
+	 * SOLID visa escrever um código que é fácil de manter e principalmente fácil de
+	 * estender, de adicionar novas características, novos comportamentos, novas
 	 * funcionalidades com um menor impacto possível.
 	 * 
 	 * Então esses princípios visam justamente simplificar esse processo de
 	 * manutenção no seu código.
 	 */
-	
+
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
-	
-	private ReajusteService reajusteService;
-
-	private List<ValidacaoReajuste> list = new ArrayList<ValidacaoReajuste>();
-
-	private ValidacaoPercentualReajuste validacaoPercentualReajuste = new ValidacaoPercentualReajuste();
-
-	private ValidacaoPeriodicidadeEntreReajustes validacaoPeriodicidadeEntreReajustes = new ValidacaoPeriodicidadeEntreReajustes();
-
 	@Autowired
 	private PromocaoService promocaoService;
-
-	ObjectMapper mapper = new ObjectMapper();
+	private ReajusteService reajusteService;
+	private List<ValidacaoReajuste> list = new ArrayList<ValidacaoReajuste>();
+	private ValidacaoPercentualReajuste validacaoPercentualReajuste = new ValidacaoPercentualReajuste();
+	private ValidacaoPeriodicidadeEntreReajustes validacaoPeriodicidadeEntreReajustes = new ValidacaoPeriodicidadeEntreReajustes();
+	String retorno = "";
 
 	@GetMapping("/hello-word")
 	@ResponseBody
 	public String hello() {
-		return "Hello worldiiiiiiiiii";
+		return "Hello";
 	}
 
 	@PostMapping("/reajusteSalarial")
-	public ResponseEntity<Funcionario> reajusteSalarial(@RequestBody Funcionario func) throws JsonProcessingException {
+	public ResponseEntity<String> reajusteSalarial(@RequestBody Funcionario func) throws JsonProcessingException {
 		list.add(validacaoPercentualReajuste);
 		list.add(validacaoPeriodicidadeEntreReajustes);
 
-		reajusteService = new ReajusteService(list,funcionarioRepository);
-		Funcionario retorno = reajusteService.reajustarSalarioDoFuncionario(func, BigDecimal.ONE);
+		reajusteService = new ReajusteService(list, funcionarioRepository);
+		Funcionario funcResponse = reajusteService.reajustarSalarioDoFuncionario(func, BigDecimal.ONE);
 
+		if (funcResponse != null) {
+			retorno = retorno.concat("funcionario atualizado com sucesso");
+		}
 		return new ResponseEntity<>(retorno, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/promocao")
-	public ResponseEntity<Funcionario> promocao(@RequestBody Funcionario func) throws JsonProcessingException {
-		Funcionario retorno = promocaoService.promover(func, true);
+	public ResponseEntity<String> promocao(@RequestBody Funcionario func) throws JsonProcessingException {
 
+		Funcionario funcResponse = promocaoService.promover(func, true);
+
+		if (funcResponse != null) {
+			retorno = retorno.concat("funcionario atualizado com sucesso");
+		}
 		return new ResponseEntity<>(retorno, HttpStatus.CREATED);
 	}
 
